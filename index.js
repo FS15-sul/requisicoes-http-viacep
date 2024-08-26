e07300481654b6c5787ac624b50e3c6d82e45754
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-08-26 11:41:45 -0300

    requisições http para o via cep

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function buscarCep(){
+    if(cep.value.length == 8){
+        fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(response => response.json())
+        .then((response) => {
+            logradouro.value = response.logradouro;
+            bairro.value = response.bairro;
+            localidade.value = response.localidade;
+            uf.value = response.uf;
+        })
+        .catch((erros) => {
+            console.log(erros)
+        })
+        .finally(() => {
+            console.log("Finalização");
+        })
+    }
+}
+
+
+// logradouro.value = "rua dos goitacases"
+// bairro.value = "meu bairro"
+
+
+
+
+
